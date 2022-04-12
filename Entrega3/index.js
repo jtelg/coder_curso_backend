207fc304ed955b5d4b9dc6c300939da80705e80a
Julian <jtelgarecz@gmail.com>
2022-04-12 16:40:21 -0300

    Finish

--- Entrega3/index.js
+++ Entrega3/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const path = require('path');
 // const product = require("./data/productos.json");
 
 const PORT = process.env.PORT || 8080;
@@ -28,7 +29,9 @@ app.get("/productoRandom", (req, res) => {
     randomProd = products[Math.floor(Math.random() * products.length)];
     res.status(200).send(`
       <pre style="font-family: Courier;background: #f4f4f4;border: solid 1px #e1e1e1;float: left;width: 100%;">
-        ${JSON.stringify(randomProd, null, " ").replace("[", "").replace("]", "")}
+        ${JSON.stringify(randomProd, null, " ")
+          .replace("[", "")
+          .replace("]", "")}
       </pre>`);
   } catch (error) {
     res.status(400).send({
@@ -37,6 +40,10 @@ app.get("/productoRandom", (req, res) => {
     });
   }
 });
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 app.listen(PORT, () => {
   console.log("server Run");
